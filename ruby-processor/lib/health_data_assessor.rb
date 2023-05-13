ef294d5c578371440d5c9f6449d18bfe7b514c23
Yoom Lam <yoom@navapbc.com>
2023-05-13 18:37:40 -0500

    updates health_data_assessor.rb

--- ruby-processor/lib/health_data_assessor.rb
+++ ruby-processor/lib/health_data_assessor.rb
@@ -4,7 +4,17 @@ class HealthDataAssessor
   def assess(contention, bp_observations)
     case contention
     when 'hypertension'
-      LighthouseObservationData.new(bp_observations).transform
+      return {} if medications.blank?
+
+      transformed_medications = LighthouseObservationData.new(bp_observations).transform
+      flagged_medications = transformed_medications.map do |medication|
+        {
+          **medication,
+          flagged: ASTHMA_KEYWORDS.any? { |keyword| medication.to_s.downcase.include?(keyword) }
+        }
+      end
+      medications = flagged_medications.sort_by { |medication| medication[:flagged] ? 0 : 1 }
+      { medications: medications }
     else
       { error: "Unsupported contention: #{contention}" }
     end
